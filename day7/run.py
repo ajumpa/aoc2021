aadee61fa8824a44f00d502657bb57e8dfc23a48
ajumpa <jumpaa@wwu.edu>
2021-12-22 17:12:00 -0800

    day7 stuff

--- day7/run.py
+++ day7/run.py
@@ -8,7 +8,10 @@ def parse_input(f):
     return np.array(f_input).astype(int)
 
 def calc_distances(map):
-    print(map)
+    n = np.shape(map)[0]
+    for ix in range(1,n):
+        map[ix,1:] = np.absolute(map[0,1:] - map[ix,0])
+    return map
 
 def create_map(input):
     n = np.size(input)
@@ -17,13 +20,28 @@ def create_map(input):
     map[0,1::] = input
     return calc_distances(map)
 
+def trace_map(map):
+    n = np.shape(map)[0]-1
+    iy = 1
+    min_sum = np.inf
+    min_x = 1
+    for row in map[1:,:]:
+        sum = np.sum(row[1:])
+        if sum < min_sum:
+            min_sum = sum
+            min_x = row[0]
+    print(min_x)
+            
+    
 def part_one(input):
     map = create_map(input)
+    print(map)
+    trace_map(map)
 
 f = open("input2", 'r')
 
-#331 too low
-
+#331 TOO LOW
+#1650 TOO LOW
 input = parse_input(f)
 input = np.sort(input)
 part_one(input)
